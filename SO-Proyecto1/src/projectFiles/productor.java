079043bd20a744ea0ec37d8835644a746cdfda20
nicoabb <nicolas.briceno@correo.unimet.edu.ve>
2022-02-17 00:14:35 -0400

    Atributos y métodos de cada clase. Librería agregada
    Falta comenzar a programar, y verificar que funciona la librería

--- SO-Proyecto1/src/projectFiles/productor.java
+++ SO-Proyecto1/src/projectFiles/productor.java
@@ -7,8 +7,62 @@ package projectFiles;
 
 /**
  *
- * @author Nicolás Briceño
+ * @author Nicolás Briceño y Christian Behrens
  */
 public class productor {
+    
+    private int storage;
+    private int maxStorage;
+    private int quantity;
+    private int time;
+    private int numProducers;
+    private int maxProducers;
+
+    public productor(int storage, int maxStorage, int quantity, int time, int numProducers, int maxProducers) {
+        
+        this.storage = storage;
+        this.maxStorage = maxStorage;
+        this.quantity = quantity;
+        this.time = time;
+        this.numProducers = numProducers;
+        this.maxProducers = maxProducers;
+    }
+    
+    public void callAssembler() {
+        
+    }
+
+    public int getStorage() {
+        return storage;
+    }
+
+    public void setStorage(int storage) {
+        this.storage = storage;
+    }
+
+    public int getNumProducers() {
+        return numProducers;
+    }
+
+    public void setNumProducers(int numProducers) {
+        this.numProducers = numProducers;
+    }
+
+    public int getMaxStorage() {
+        return maxStorage;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public int getMaxProducers() {
+        return maxProducers;
+    }
+    
     
 }
